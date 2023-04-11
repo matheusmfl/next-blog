36a40659f02bdb0d5071fb159729c8b10509c725
Matheus Fonteles <92957086+matheusmfl@users.noreply.github.com>
2023-04-10 23:08:51 -0300

    Config the SanityV3 CMS Api

---
+++ white-ant/sanity.config.ts
@@ -0,0 +1,27 @@
+import {defineConfig, isDev} from 'sanity'
+import {visionTool} from '@sanity/vision'
+import {deskTool} from 'sanity/desk'
+import {schemaTypes} from './schemas'
+import {getStartedPlugin} from './plugins/sanity-plugin-tutorial'
+
+const devOnlyPlugins = [getStartedPlugin()]
+
+export default defineConfig({
+  name: 'default',
+  title: 'white-ant',
+  templates: [
+    {
+      name: 'blog',
+      path: 'templates/blog.js',
+    },
+  ],
+
+  projectId: 'mx80fx9y',
+  dataset: 'production',
+
+  plugins: [deskTool(), visionTool(), ...(isDev ? devOnlyPlugins : [])],
+
+  schema: {
+    types: schemaTypes,
+  },
+})
