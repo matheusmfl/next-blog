599d4a85eb9f07e52b2ed6076aa22b7e7aca6c49
Matheus Fonteles <92957086+matheusmfl@users.noreply.github.com>
2023-04-19 14:18:48 -0300

    Config the visualization posts on homePage

--- src/components/BlogList.tsx
+++ src/components/BlogList.tsx
@@ -2,7 +2,7 @@
 
 import urlFor from "@/lib/urlFor"
 import Image from "next/image"
-import category from "../../schemas/category"
+import { ArrowRightIcon } from "@heroicons/react/24/solid"
 
 type props = {
     posts: Post[]
@@ -16,7 +16,7 @@ export default function BlogList({ posts }: props) {
             <div>
                 <hr className="border-[#f7AB0A] mb-10" />
 
-                <div>
+                <div className="grid grid-cols-1 md:grid-cols-2 px-10 gap-10 gap-y-16 pb-24">
                     {posts.map(post => {
                         return (
                             <div key={post._id} className="flex flex-col group cursor-pointer">
@@ -51,6 +51,10 @@ export default function BlogList({ posts }: props) {
                                     <p className="underline text-lg font-bold">{post.title}</p>
                                     <p className="text-gray-500 line-clamp-2">{post.description}</p>
                                 </div>
+                                <p className="mt-5 font-bold flex items-center group-hover:underline">Ler esse artigo
+
+                                    <ArrowRightIcon className="ml-2 h-4 w-4" />
+                                </p>
                             </div>
                         )
                     })}
